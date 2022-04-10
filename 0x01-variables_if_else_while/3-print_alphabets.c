605e698f8951ec1b2df89661ad652cbe3d1e584d
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-10 15:21:41 -0700

    my 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char alphABET;
+
+for (alphABET = 'a', 'A'; alphABET <= 'z', 'Z'; alphABET++)
+{
+putchar(alphABET);
+}
+
+putchar('\n');
+
+return (0)
+
+}
