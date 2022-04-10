771d96b5e576d79468f20ace4893c6bfe784d843
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-10 15:03:05 -0700

    my 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 
 {
 char alphabet;
 
-for(alphabet = "a"; alphabet <= "z"; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
