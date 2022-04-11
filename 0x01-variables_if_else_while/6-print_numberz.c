c46f167d5c8fca2cb8554348d2066b60fe79978e
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-10 17:36:35 -0700

    my 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,15 +5,16 @@
  * Return: Always 0 (success)
  */
 int main(void)
+
 {
 int i;
 
-for (i = 35; i <= 44; i++)
+for (i = 48 ; i <=57; i++)
 {
-putchar (i);
+putchar (i) ;
 }
 
-putchar ("\n");
+putchar ('\n');
 
 return (0);
 
