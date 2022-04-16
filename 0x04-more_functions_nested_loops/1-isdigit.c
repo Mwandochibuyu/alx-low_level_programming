d84a57729cb17696aeed97d58b6e93af7c157633
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-16 14:55:14 -0700

    my 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - evalue if letter is a digit
+ * @c: print int
+ *Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+for (c = 0; c <= 10; c++)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
