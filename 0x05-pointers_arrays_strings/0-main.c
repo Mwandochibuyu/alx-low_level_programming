2dd06bf7b26c718dae96cd90109f6dd6efa66e1f
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-20 07:08:01 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+int n = 98;
+int *p = &n;
+
+{
+_putchar("%d\n", n);
+reaturn (0);
+}
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+int a = 98;
+int *n = &a;
+
+{
+_putchar("%d\n", *n);
+return (0);
+}
+}
