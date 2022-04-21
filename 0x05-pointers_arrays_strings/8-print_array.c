ec063c74896503faededa6782fdde19b7b38282e
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-20 19:33:36 -0700

    my 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,12 +15,12 @@ void print_array(int *a, int n)
 
 	i = 0;
 
-	for (n--; n >= 0; n--, i ++)
+	for (n--; n >= 0; n--; i++)
 	{
 	printf("%d", a[i]);
 	if (n > 0)
 	{
-	printf(",");
+	printf(" , ");
 	}
 	}
 	printf("\n");
