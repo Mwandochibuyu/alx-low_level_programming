008872c9108ca97ef45d68d3f3271eb21cab5098
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-24 19:36:35 -0700

    my commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; desk[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	for (j = 0; j < n; j++)
-	desk[i] = src[j];
+	dest[i] = src[j];
 	i++;
 	}
-	return (desk);
+	return (dest);
 
 }
