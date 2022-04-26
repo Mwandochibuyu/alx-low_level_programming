1b39812d77c568f3321de7e2c123df47623e69f8
Mwandochibuyu <mwandoenoch@gmail.com>
2022-04-25 18:30:26 -0700

    my commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,28 @@
+
 #include "main.h"
+
 /**
- * _strcat - concatinate two string
- * @dest: first string
- * @src: second string
- * Return: string
+ *_strcat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *Return: string.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	for (j = 0; src[j] != '\0'; j++)
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
