879e04b73d7dbdb279e7ecd3fa263e98163bf689
Mwandochibuyu <mwandoenoch@gmail.com>
2022-05-04 13:51:54 -0700

    my commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * whatsmyanme - writing a program that prints its name, followed by a new line
+ * @argc: prints the total number of arguments supplied through command line
+ * @argv: is an array of character type
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+_putchar("%s\n", argv[0]);
+return (0);
+}
